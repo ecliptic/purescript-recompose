5d933490937889a46eb1a644153d1fd3dc787785
bkonkle <brandon@konkle.us>
2016-08-07 15:38:21 -0600

    Update to get rid of as many uncurried functions as possible

--- src/Recompose.js
+++ src/Recompose.js
@@ -3,31 +3,99 @@
 var recompose = require('recompose')
 
 exports.mapProps = recompose.mapProps
+
 exports.withProps = recompose.withProps
-exports.withPropsOnChange = recompose.withPropsOnChange
+
+exports.withPropsOnChange = function (shouldMapOrKeys) {
+  return function (createProps) {
+    return recompose.withPropsOnChange(shouldMapOrKeys, createProps)
+  }
+}
+
 exports.withHandlers = recompose.withHandlers
+
 exports.defaultProps = recompose.defaultProps
-exports.renameProp = recompose.renameProp
+
+exports.renameProp = function (oldName) {
+  return function (newName) {
+    return recompose.renameProp(oldName, newName)
+  }
+}
+
 exports.renameProps = recompose.renameProps
+
 exports.flattenProp = recompose.flattenProp
-exports.withState = recompose.withState
-exports.withReducer = recompose.withReducer
-exports.branch = recompose.branch
+
+exports.withState = function (stateName) {
+  return function (stateUpdaterName) {
+    return function (initialState) {
+      return recompose.withState(stateName, stateUpdaterName, initialState)
+    }
+  }
+}
+
+exports.withReducer = function (stateName) {
+  return function (dispatchName) {
+    return function (reducer) {
+      return function (initialState) {
+        return recompose.withReducer(stateName, dispatchName, reducer, initialState)
+      }
+    }
+  }
+}
+
+exports.branch = function (test) {
+  return function (left) {
+    return function (right) {
+      return recompose.branch(test, left, right)
+    }
+  }
+}
+
 exports.renderComponent = recompose.renderComponent
+
 exports.renderNothing = recompose.renderNothing
+
 exports.shouldUpdate = recompose.shouldUpdate
+
 exports.pure = recompose.pure
+
 exports.onlyUpdateForKeys = recompose.onlyUpdateForKeys
+
 exports.onlyUpdateForPropTypes = recompose.onlyUpdateForPropTypes
-exports.withContext = recompose.withContext
+
+exports.withContext = function (childContextTypes) {
+  return function (getChildContext) {
+    return recompose.withContext(childContextTypes, getChildContext)
+  }
+}
+
 exports.getContext = recompose.getContext
+
 exports.lifecycle = recompose.lifecycle
+
 exports.toClass = recompose.toClass
-exports.setStatic = recompose.setStatic
+
+exports.setStatic = function (key) {
+  return function (value) {
+    return recompose.setStatic(key, value)
+  }
+}
+
 exports.setPropTypes = recompose.setPropTypes
+
 exports.setDisplayName = recompose.setDisplayName
+
 exports.getDisplayName = recompose.getDisplayName
-exports.wrapDisplayName = recompose.wrapDisplayName
+
+exports.wrapDisplayName = function (component) {
+  return function (wrapperName) {
+    return recompose.wrapDisplayName(component, wrapperName)
+  }
+}
+
 exports.componentFromProp = recompose.componentFromProp
+
 exports.nest = recompose.nest
+
 exports.hoistStatics = recompose.hoistStatics
--- src/Recompose.purs
+++ src/Recompose.purs
@@ -2,82 +2,113 @@ module Recompose
   ( ReactFunctionalComponent
   , HigherOrderComponent
   , mapProps
+  , withProps
+  , withPropsOnChange
+  , withHandlers
+  , defaultProps
+  , renameProp
+  , renameProps
+  , flattenProp
+  , withState
+  , withReducer
+  , branch
+  , renderComponent
+  , renderNothing
+  , shouldUpdate
+  , pure
+  , onlyUpdateForKeys
+  , onlyUpdateForPropTypes
   , withContext
+  , getContext
+  , lifecycle
+  , toClass
+  , setStatic
+  , setPropTypes
+  , setDisplayName
+  , getDisplayName
+  , wrapDisplayName
+  , componentFromProp
+  , nest
+  , hoistStatics
   ) where
 
-import Data.Function.Uncurried (Fn1, Fn2, Fn3, Fn4)
+import Data.Function.Uncurried (Fn2)
 import React (ReactClass, ReactElement)
 
-type ReactFunctionalComponent = forall props. Fn1 props ReactElement
-type HigherOrderComponent = forall props. Fn1 (ReactClass props) ReactFunctionalComponent
+type ReactFunctionalComponent = forall props. props -> ReactElement
+type HigherOrderComponent = forall props.
+  ReactClass props -> ReactFunctionalComponent
 
 foreign import mapProps :: forall ownerProps props.
-  Fn1 (Fn1 ownerProps props) HigherOrderComponent
+  (ownerProps -> props) -> HigherOrderComponent
 
 foreign import withProps :: forall ownerProps props.
-  Fn1 (Fn1 ownerProps props) HigherOrderComponent
+  (ownerProps -> props) -> HigherOrderComponent
 
 foreign import withPropsOnChange :: forall ownerProps props.
-  Fn2 (Array String) (Fn1 ownerProps props) HigherOrderComponent
+  Array String -> (ownerProps -> props) -> HigherOrderComponent
 
 foreign import withHandlers :: forall handlerCreators.
-  Fn1 handlerCreators HigherOrderComponent
+  handlerCreators -> HigherOrderComponent
 
-foreign import defaultProps :: forall props. Fn1 props HigherOrderComponent
+foreign import defaultProps :: forall props. props -> HigherOrderComponent
 
-foreign import renameProp :: Fn2 String String HigherOrderComponent
+foreign import renameProp :: String -> String -> HigherOrderComponent
 
-foreign import renameProps :: forall nameMap. Fn1 nameMap HigherOrderComponent
+foreign import renameProps :: forall nameMap. nameMap -> HigherOrderComponent
 
-foreign import flattenProp :: Fn1 String HigherOrderComponent
+foreign import flattenProp :: String -> HigherOrderComponent
 
 foreign import withState :: forall initialState props.
-  Fn3 String String (Fn1 props initialState) HigherOrderComponent
+  String -> String -> (props -> initialState) -> HigherOrderComponent
 
 foreign import withReducer :: forall state action.
-  Fn4 String String (Fn2 state action state) state HigherOrderComponent
+  String -> String -> (Fn2 state action state) -> state -> HigherOrderComponent
 
 foreign import branch :: forall props.
-  Fn3 (Fn1 props Boolean) HigherOrderComponent HigherOrderComponent HigherOrderComponent
+  (props -> Boolean) -> HigherOrderComponent -> HigherOrderComponent ->
+  HigherOrderComponent
 
-foreign import renderComponent :: forall props. Fn1 (ReactClass props) HigherOrderComponent
+foreign import renderComponent :: forall props.
+  ReactClass props -> HigherOrderComponent
 
 foreign import renderNothing :: HigherOrderComponent
 
 foreign import shouldUpdate :: forall props nextProps.
-  Fn1 (Fn2 props nextProps Boolean) HigherOrderComponent
+  (Fn2 props nextProps Boolean) -> HigherOrderComponent
 
 foreign import pure :: HigherOrderComponent
 
-foreign import onlyUpdateForKeys :: Fn1 (Array String) HigherOrderComponent
+foreign import onlyUpdateForKeys :: Array String -> HigherOrderComponent
 
 foreign import onlyUpdateForPropTypes :: HigherOrderComponent
 
 foreign import withContext :: forall props childContextTypes childContext.
-  Fn2 childContextTypes (Fn1 props childContext) HigherOrderComponent
+  childContextTypes -> (props -> childContext) -> HigherOrderComponent
 
 foreign import getContext :: forall contextTypes.
-  Fn1 contextTypes HigherOrderComponent
+  contextTypes -> HigherOrderComponent
 
-foreign import lifecycle :: forall spec. Fn1 spec HigherOrderComponent
+foreign import lifecycle :: forall spec. spec -> HigherOrderComponent
 
 foreign import toClass :: HigherOrderComponent
 
-foreign import setStatic :: forall value. Fn2 String value HigherOrderComponent
+foreign import setStatic :: forall value.
+  String -> value -> HigherOrderComponent
 
 foreign import setPropTypes :: forall propTypes.
-  Fn1 propTypes HigherOrderComponent
+  propTypes -> HigherOrderComponent
 
-foreign import setDisplayName :: Fn1 String HigherOrderComponent
+foreign import setDisplayName :: String -> HigherOrderComponent
 
-foreign import getDisplayName :: forall component. Fn1 component String
+foreign import getDisplayName :: forall component. component -> String
 
 foreign import wrapDisplayName :: forall component.
-  Fn2 component String String
+  component -> String -> String
 
-foreign import componentFromProp :: Fn1 String ReactFunctionalComponent
+foreign import componentFromProp :: String -> ReactFunctionalComponent
 
 foreign import nest :: forall component props.
-  Fn1 (Array component) (ReactClass props)
+  Array component -> ReactClass props
 
-foreign import hoistStatics :: Fn1 HigherOrderComponent HigherOrderComponent
+foreign import hoistStatics :: HigherOrderComponent -> HigherOrderComponent
